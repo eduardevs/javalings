f196493ec4f36ae4e2afcaafc4f1bb38ee8e57a4
eduardevs <jeseduardopi@gmail.com>
2024-05-29 23:41:34 +0200

    Queue structure class js reference

---
+++ Algorithms/LinkedList/QueueJs.js
@@ -0,0 +1,16 @@
+export default Queue class Queue<T> {
+	private tail?: Node<T>;
+
+	constructor() {
+		this.head = this.tail = undefined;
+		this.length = 0;
+	}
+
+	// enqueue
+
+	// deque
+
+	// peek
+
+
+}
